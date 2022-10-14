165f649acfe2d3839b693e548d38c4fe3617715a
viniciustxc3 <viniciustxc@yahoo.com.br>
2022-10-14 15:16:15 -0300

    CNN Autoencoder

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ pip install -r requirements.txt # baixar
 '''
 
 # Library necessary
+from tensorflow import keras
+from tensorflow.keras import layers
+from pickletools import optimize
 from keras.layers import Dense, Conv2D, MaxPool2D, UpSampling2D
 from keras import Input, Model
 from keras.datasets import mnist
@@ -13,65 +16,117 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-'''
-Construção do modelo:
-*** Quanto menor a dimensão, maior será a compressão
-'''
-encoding_dim = 15 # numero de dimensoes que decideo quanto a entrada será compactada
-input_img = Input(shape=(784,))
-# Representação codificada
-encoded = Dense(encoding_dim, activation='relu')(input_img)
-# Representação decodificada
-decoded = Dense(784, activation='sigmoid')(encoded)
-# Modelo que pega a imagem de entrada e motra a decodificação da imagem
-autoencoder = Model(input_img, decoded)
+# '''
+# Construção do modelo:
+# *** Quanto menor a dimensão, maior será a compressão
+# '''
+# encoding_dim = 15 # numero de dimensoes que decideo quanto a entrada será compactada
+# input_img = Input(shape=(784,))
+# # Representação codificada
+# encoded = Dense(encoding_dim, activation='relu')(input_img)
+# # Representação decodificada
+# decoded = Dense(784, activation='sigmoid')(encoded)
+# # Modelo que pega a imagem de entrada e motra a decodificação da imagem
+# autoencoder = Model(input_img, decoded)
 
-'''
-Construção do modelo do codificador e decodificador separados para facilitar 
-a diferença entre a entrada e a saída
-'''
-# Modelo de imagens codificadas
-encoder = Model(input_img, encoded)
-# Criação do modelo decodificado
-encoded_input = Input(shape=(encoding_dim,))
-# Ultima camada do modelo autoencoder
-decoder_layer = autoencoder.layers[-1]
-# Modelo decoder
-decoder = Model(encoded_input, decoder_layer(encoded_input))
+# '''
+# Construção do modelo do codificador e decodificador separados para facilitar 
+# a diferença entre a entrada e a saída
+# '''
+# # Modelo de imagens codificadas
+# encoder = Model(input_img, encoded)
+# # Criação do modelo decodificado
+# encoded_input = Input(shape=(encoding_dim,))
+# # Ultima camada do modelo autoencoder
+# decoder_layer = autoencoder.layers[-1]
+# # Modelo decoder
+# decoder = Model(encoded_input, decoder_layer(encoded_input))
 
-'''
-Compilando modelo com o otimizador ADAM
-E ajuste da função de perda de entropia cruzada
-'''
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+# '''
+# Compilando modelo com o otimizador ADAM
+# E ajuste da função de perda de entropia cruzada
+# '''
+# autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
 
-# Carregando os dados
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-print(x_train.shape)
-print(x_test.shape)
+# # Carregando os dados
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+# x_train = x_train.astype('float32') / 255.
+# x_test = x_test.astype('float32') / 255.
+# x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+# x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+# print(x_train.shape)
+# print(x_test.shape)
 
-# Ver como os dados estão em plot
-plt.imshow(x_train[0].reshape(28,28))
+# # Ver como os dados estão em plot
+# plt.imshow(x_train[0].reshape(28,28))
+# # plt.show()
+
+# # Treinando o sinal
+# autoencoder.fit(x_train, x_train,
+#                 epochs=15,
+#                 batch_size=256,
+#                 validation_data=(x_test,x_test))
+
+# # Fornecer entrada e plotar resultados
+# encoded_img = encoder.predict(x_test)
+# decoded_img = decoder.predict(encoded_img)
+# plt.figure(figsize=(20,4))
+# for i in range(5):
+#     # Display original
+#     ax = plt.subplot(2, 5, i + 1)
+#     plt.imshow(x_test[i].reshape(28,28))
+#     plt.gray()
+#     ax.get_xaxis().set_visible(False)
+#     ax.get_yaxis().set_visible(False)
 # plt.show()
 
-# Treinando o sinal
-autoencoder.fit(x_train, x_train,
-                epochs=15,
-                batch_size=256,
-                validation_data=(x_test,x_test))
+#####################################################################3
 
-# Fornecer entrada e plotar resultados
-encoded_img = encoder.predict(x_test)
-decoded_img = decoder.predict(encoded_img)
-plt.figure(figsize=(20,4))
+'''
+Deep CNN Autoencoder:
+
+Como estamos usando imagens, faz sentido usar uma rede neural convolucional (CNN).
+Composto por um vetor de camadas Conv2D e max-pooling e o decodificador será um vetor Conv2D e Upsampling.
+'''
+model = keras.Sequential()
+# encoder network
+model.add(Conv2D(30, 3, activation='relu', padding='same', input_shape = (28,28,1)))
+model.add(MaxPool2D(2, padding = 'same'))
+model.add(Conv2D(15, 3, activation= 'relu', padding='same'))
+model.add(MaxPool2D(2, padding = 'same'))
+# decoder network
+model.add(Conv2D(15, 3, activation= 'relu', padding='same'))
+model.add(UpSampling2D(2))
+model.add(Conv2D(30, 3, activation= 'relu', padding='same'))
+model.add(UpSampling2D(2))
+model.add(Conv2D(1, 3, activation= 'sigmoid', padding='same')) # output layer
+model.compile(optimizer= 'adam', loss= 'binary_crossentropy')
+model.summary()
+
+# Carregando dados e treinando o modelo
+(x_train, _), (x_test, _) = mnist.load_data()
+x_train = x_train.astype('float')/255.
+x_test = x_test.astype('float')/255.
+x_train = np.reshape(x_train,  (len(x_train), 28, 28, 1))
+x_test = np.reshape(x_test,  (len(x_test), 28, 28, 1))
+model.fit(x_train, x_train,
+                epochs=15,
+                batch_size=128,
+                validation_data=(x_test, x_test))
+
+# Fornecendo o input e gerando a saída dos resultados
+pred = model.predict(x_test)
+plt.figure(figsize=(20, 4))
 for i in range(5):
     # Display original
     ax = plt.subplot(2, 5, i + 1)
-    plt.imshow(x_test[i].reshape(28,28))
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    # Display reconstruction
+    ax = plt.subplot(2, 5, i + 1 + 5)
+    plt.imshow(pred[i].reshape(28, 28))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
